3541b35f68afb688f66f81aa5c83032459902ad5
Pranay Chauhan <cprince4742@gmail.com>
2020-07-31 16:56:26 +0530

    Auth , Queries & Mutation

---
+++ task_manager/main/gql/query.py
@@ -0,0 +1,32 @@
+import graphene
+from .typeDef import TaskType
+from main.models import Task
+from django.core.exceptions import ObjectDoesNotExist
+from .typeDefs import UserType
+from django.contrib.auth.models import User
+
+
+class Query:
+    all_tasks = graphene.List(TaskType)
+    task = graphene.Field(TaskType, id=graphene.ID())
+    all_users = graphene.List(UserType)
+    user = graphene.Field(UserType, id=graphene.Int())
+
+
+    def resolve_all_tasks(self, info, **kwargs):
+  	    return Task.objects.all()
+
+    def resolve_task(self, info, **kwargs):
+        try:
+            return Task.objects.get(pk=kwargs.get('id'))
+        except ObjectDoesNotExist:
+            return None    
+
+    def resolve_all_users(sef,info,**kwargs):
+        return User.objects.all()
+
+    def resolve_user(self,info,**kwargs):
+        try:
+            return User.objects.get(pk=kwargs.get('id'))    
+        except ObjectDoesNotExist:
+            return None    
